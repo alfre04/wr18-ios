46a4f78e41f3e62d59af939fe42a7651af083cbc
Alfredo Govea <agovea@mexltagovea-3.local>
2018-02-07 16:30:19 -0600

    Create app iOS

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'ios',
+		wp_ws_url : 'http://warroom2018.com/wp-appkit-api/ios',
+		wp_url : 'http://warroom2018.com',
+		theme : 'wpak-theme-bootstrap-feature-user-login-ios',
+		version : '1.0',
+		app_title : 'WR2018',
+		app_platform : 'ios',
+		gmt_offset : -6,
+		debug_mode : 'off',
+		auth_key : 'put your unique phrase here',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
